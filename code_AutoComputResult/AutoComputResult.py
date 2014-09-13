94012e12654b32e420db56b4111e4bf957868605
jetxl <mingyun.xl@163.com>
2014-09-13 17:41:37 +0800

    v1.0
    test

--- code_AutoComputResult/AutoComputResult.py
+++ code_AutoComputResult/AutoComputResult.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import os
 
 #---------------------------------
@@ -10,6 +10,31 @@ import os
 
 
 print "Now is computing result....."
-
 class AutoComputResult:
+	#结果数据的格式(后缀)
+	perfmanNFormat="##other##.perf.detail.txt"
+	perfmanPFormat=".perf.detail.txt"
+	mttesterFormat=""
+	mtrecFormat=""
+
+	#结果数据的路径
+	resourcePath="C:\\Users\\Administrator\\Desktop\\性能测试工具\\AutoComputResult\\resource\\result_source\\Perfman\\"
+	resourcePath=unicode(resourcePath,"utf-8")
 	
+	#用于统计暂存数据
+	BytesReceived	=	0
+	BytesSent		=	0      
+	BytesTotal		=	0
+	Handle count        Thread count        Working Set         % Processor Time    private Bytes
+	perfmanProcess=[];
+
+
+	def __int__(self):
+		print "this is init\n"
+		
+
+	def getPerfmanN(self):
+		print self.resourcePath+self.perfmanNFormat+"\n"
+		perfmanNfile=open(self.resourcePath+self.perfmanNFormat,"r")
+		resource=perfmanNfile.read()
+		print resource[0]
---
+++ code_AutoComputResult/main.py
@@ -0,0 +1,6 @@
+# -*- coding: cp936 -*-  
+from AutoComputResult import *
+
+
+COMput=AutoComputResult()
+COMput.getPerfmanN()
